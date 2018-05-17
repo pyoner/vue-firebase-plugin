aa5ce1d47f835b3607454360d7520df47c9ed75f
Jungle <devex.soft@gmail.com>
2018-05-17 11:31:01 +0500

    Add dummy plugin

--- src/vue-firebase-plugin.ts
+++ src/vue-firebase-plugin.ts
@@ -1,4 +1,5 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+// ..
+import _Vue from 'vue'
+export default function install(Vue: typeof _Vue, options?: any): void {}
