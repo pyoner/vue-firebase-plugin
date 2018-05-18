c566b09dcc6235fe3453a273199825915a23f68f
Jungle <devex.soft@gmail.com>
2018-05-18 12:28:04 +0500

    feat(firebase): Add firebase app config

--- src/vue-firebase-plugin.ts
+++ src/vue-firebase-plugin.ts
@@ -2,4 +2,31 @@
 // import "core-js/fn/array.find"
 // ..
 import _Vue from 'vue'
-export default function install(Vue: typeof _Vue, options?: any): void {}
+import firebase from 'firebase'
+import '@firebase/firestore'
+import { FirebaseOptions } from '@firebase/app-types'
+
+export default function install(Vue: typeof _Vue, options: Options): void {
+  const app = firebase.initializeApp(options.config)
+  Vue.prototype.$firebaseApp = app
+
+  // firestore
+  // if (options.firestore) {
+  //   const firestore = firebase.firestore();
+  //   const { settings, logLevel } = options.firestore;
+
+  //   if (settings) {
+  //     firestore.settings(settings);
+  //   }
+
+  //   if (logLevel) {
+  //     // firestore.setLogLevel(logLevel);
+  //   }
+  //   // register Firestore component as global component
+  //   // Vue.component(Firestore.name, Firestore);
+  // }
+}
+
+export interface Options {
+  config: FirebaseOptions
+}
