739661c4afd8a16f4e01b0f922e41fee680b7f43
Jungle <devex.soft@gmail.com>
2018-05-18 12:39:30 +0500

    feat(firestore): Add firestore config

--- src/vue-firebase-plugin.ts
+++ src/vue-firebase-plugin.ts
@@ -2,31 +2,36 @@
 // import "core-js/fn/array.find"
 // ..
 import _Vue from 'vue'
-import firebase from 'firebase'
-import '@firebase/firestore'
+import firebase, { firestore } from 'firebase'
 import { FirebaseOptions } from '@firebase/app-types'
+import '@firebase/firestore'
+import { Settings, LogLevel } from '@firebase/firestore-types'
 
 export default function install(Vue: typeof _Vue, options: Options): void {
   const app = firebase.initializeApp(options.config)
   Vue.prototype.$firebaseApp = app
 
   // firestore
-  // if (options.firestore) {
-  //   const firestore = firebase.firestore();
-  //   const { settings, logLevel } = options.firestore;
+  if (options.firestore) {
+    const { settings, logLevel } = options.firestore
+    if (logLevel) {
+      firebase.firestore.setLogLevel(logLevel)
+    }
 
-  //   if (settings) {
-  //     firestore.settings(settings);
-  //   }
+    const firestore = firebase.firestore()
+    if (settings) {
+      firestore.settings(settings)
+    }
 
-  //   if (logLevel) {
-  //     // firestore.setLogLevel(logLevel);
-  //   }
-  //   // register Firestore component as global component
-  //   // Vue.component(Firestore.name, Firestore);
-  // }
+    // register Firestore component as global component
+    // Vue.component(Firestore.name, Firestore);
+  }
 }
 
 export interface Options {
   config: FirebaseOptions
+  firestore: {
+    settings?: Settings
+    logLevel?: LogLevel
+  }
 }
