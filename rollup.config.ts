cecfc82f6491f9e5dd964732fd1ed8a29349d5d9
Jungle <devex.soft@gmail.com>
2018-05-22 10:40:30 +0500

    build(rollup): Fix external, commonjs, resolve

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,11 +4,11 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-import vue from 'rollup-plugin-vue'
 
 const pkg = require('./package.json')
 
 const libraryName = 'vue-firebase-plugin'
+const external = Object.keys(pkg.dependencies || {})
 
 export default {
   input: `src/${libraryName}.ts`,
@@ -18,23 +18,25 @@ export default {
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['vue', 'firebase'],
+  external,
   watch: {
     include: 'src/**'
   },
   plugins: [
     // Allow json resolution
     json(),
-    // Vue
-    vue(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
+    commonjs({
+      namedExports: {
+        'vue-property-decorator': ['Component', 'Prop', 'Watch']
+      }
+    }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
 
     // Resolve source maps to the original source
     sourceMaps()
