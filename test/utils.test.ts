30864fc66938706ad9a2e781524bba8db8d60b96
Jungle <devex.soft@gmail.com>
2018-05-31 14:29:44 +0500

    test(utils): Add test for utils.createRef

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,5 @@
 import * as utils from '../src/utils'
+import firebase, { firestore } from 'firebase'
 
 /**
  * utils test
@@ -6,5 +7,34 @@ import * as utils from '../src/utils'
 describe('utils test', () => {
   it('has normalizeUser', () => {
     expect(utils).toHaveProperty('normalizeUser')
+  })
+})
+
+describe('utils.createRef', () => {
+  firebase.initializeApp({
+    apiKey: 'key',
+    authDomain: 'domain',
+    databaseURL: 'database',
+    projectId: 'project id'
+  })
+
+  firestore().settings({ timestampsInSnapshots: true })
+
+  it('should return CollectionReference', () => {
+    expect(utils.createRef({ collection: 'users' })).toBeInstanceOf(
+      firestore.CollectionReference
+    )
+  })
+
+  it('should return DocumentReference', () => {
+    expect(utils.createRef({ collection: 'users', id: '3' })).toBeInstanceOf(
+      firestore.DocumentReference
+    )
+  })
+
+  it('should return Query', () => {
+    expect(
+      utils.createRef({ collection: 'users', where: [['name', '==', 'Alice']] })
+    ).toBeInstanceOf(firestore.Query)
   })
 })
